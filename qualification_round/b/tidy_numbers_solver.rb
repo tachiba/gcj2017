fc18aa270e4adcbf8d0242e8e7c9ad9c4b5ffe2f
Takashi Chiba <mail@takashi.me>
2017-04-08 22:28:31 +0900

    ✨ Submit B-large

--- qualification_round/b/tidy_numbers_solver.rb
+++ qualification_round/b/tidy_numbers_solver.rb
@@ -1,5 +1,5 @@
 class TidyNumbersSolver
-  attr_reader :max, :current
+  attr_reader :max, :current, :next_candidate
 
   def initialize(input)
     @max = input.to_i
@@ -10,7 +10,7 @@ class TidyNumbersSolver
     loop do
       return @current if _tidy?
 
-      @current -= 1
+      _jump_to_next_candidate
     end
   end
 
@@ -28,11 +28,26 @@ class TidyNumbersSolver
         next
       end
 
-      return false if prev_digit > digit
+      if prev_digit > digit
+        prev_cursor = index - 1
+        zero_length = @current.to_s.size - index
+
+        @next_candidate = (@current.to_s[0..prev_cursor] + '0' * zero_length).to_i
+
+        return false
+      end
 
       prev_digit = digit
     end
 
     true
+  end
+
+  def _jump_to_next_candidate
+    if next_candidate
+      @current = next_candidate - 1
+    else
+      @current -= 1
+    end
   end
 end
